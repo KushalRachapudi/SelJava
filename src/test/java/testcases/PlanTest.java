a0131ce0e66455c54ba66457a5114fc060e9e47c
KushalRachapudi <kushal_rkk@yahoo.com>
2023-05-23 14:51:55 +1000

    simple test

---
+++ src/test/java/testcases/PlanTest.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+
+public class PlanTest{
+
+	@Test(priority = 1)
+	public void verifyfreeTrialBtn() {
+	
+		int a=5;
+		int b=5;
+		int c=a+b;
+		System.out.println("the sum is:" + c);
+		
+	}
+
+}
